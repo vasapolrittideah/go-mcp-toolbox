05e15555a699b772c8269332305b7b62c258c2d6
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-07-29 00:58:33 +0700

    Add hotel database schema and seed

---
+++ toolbox/hotel_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE hotels(
+  id            INTEGER NOT NULL PRIMARY KEY,
+  name          VARCHAR NOT NULL,
+  location      VARCHAR NOT NULL,
+  price_tier    VARCHAR NOT NULL,
+  checkin_date  DATE    NOT NULL,
+  checkout_date DATE    NOT NULL,
+  booked        BIT     NOT NULL
+);
